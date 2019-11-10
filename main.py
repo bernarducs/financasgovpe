c69669ef290196ff19d8ab528be8f90151027d6e
bernarducs <bernarducs@gmail.com>
2019-11-10 17:17:21 -0300

    1a versao. sem a lista e iterar pelos orgaos

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from selenium import webdriver
 from time import sleep
 
-driver = webdriver.Firefox()
+dir = r'\despesa'
+
+fp = webdriver.FirefoxProfile()
+fp.set_preference("browser.download.folderList", 2)
+fp.set_preference("browser.download.manager.showWhenStarting", False)
+fp.set_preference("browser.download.dir", dir)
+fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/csv")
+fp.set_preference("dom.disable_deforeunload", True)
+
+options = webdriver.FirefoxOptions()
+# options.add_argument('-headless')
+
+driver = webdriver.Firefox(fp, options=options)
+
 url = 'http://web.transparencia.pe.gov.br/despesas/despesa-geral/'
 driver.get(url)
 sleep(10)
@@ -12,4 +25,10 @@ driver.execute_script(js)
 
 driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
 options = driver.find_elements_by_xpath("//*[@id='html_selectug']/select/option")
-options[4].click()
+
+#orgaos = [option.text for option in options]
+
+btn_exp = driver.find_element_by_id('exportTable')
+btn_exp.click()
+
+
