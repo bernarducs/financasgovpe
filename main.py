003cce590f2a575364670085736400cc7405a69f
bernarducs <bernarducs@gmail.com>
2019-11-10 21:45:06 -0300

    encapsulando em funcoes. ainda falta iterar pelos orgaos

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 from selenium import webdriver
 from time import sleep
+import os
 
-fp = webdriver.FirefoxProfile()
-fp.set_preference("browser.download.folderList", 2)
-fp.set_preference("browser.download.manager.showWhenStarting", False)
-fp.set_preference("browser.download.dir", "/extracao")
-fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.ms-excel")
-fp.set_preference("dom.disable_deforeunload", True)
 
-options = webdriver.FirefoxOptions()
-options.add_argument('-headless')
+def get_driver():
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("browser.download.folderList", 2)
+    fp.set_preference("browser.download.manager.showWhenStarting", False)
+    fp.set_preference("browser.download.dir", os.getcwd() + "/extracao/despesa")
+    fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/vnd.ms-excel")
+    fp.set_preference("dom.disable_deforeunload", True)
 
-driver = webdriver.Firefox(fp, options=options)
-url = 'http://web.transparencia.pe.gov.br/despesas/despesa-geral/'
-driver.get(url)
-sleep(10)
+    options = webdriver.FirefoxOptions()
+    # options.add_argument('-headless')
 
-js = "document.getElementById('iframe').contentWindow.document.getElementsByClassName('chzn-select')" \
-     "[1].style.display = 'inline';"
-driver.execute_script(js)
+    driver = webdriver.Firefox(fp, options=options)
+    url = 'http://web.transparencia.pe.gov.br/despesas/despesa-geral/'
+    driver.get(url)
 
-driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
-options = driver.find_elements_by_xpath("//*[@id='html_selectug']/select/option")
-options[9].click()
-sleep(5)
-# orgaos = [option.text for option in options]
+    return driver
 
-btn_table = driver.find_element_by_xpath('//*[@id="exportTable"]')
-driver.execute_script('arguments[0].scrollIntoView(true);', btn_table)
-btn_table.click()
 
-sleep(5)
-driver.close()
+def get_options(driver):
+    js = "document.getElementById('iframe').contentWindow.document.getElementsByClassName('chzn-select')" \
+         "[1].style.display = 'inline';"
+    driver.execute_script(js)
+    driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+    options = driver.find_elements_by_xpath("//*[@id='html_selectug']/select/option")
+    return options
+
+
+def extract_table(driver):
+    btn_table = driver.find_element_by_xpath('//*[@id="exportTable"]')
+    driver.execute_script('arguments[0].scrollIntoView(true);', btn_table)
+    btn_table.click()
