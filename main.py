f8b752318ed031020ab731456c03b6b41b6944e9
bernarducs <bernarducs@gmail.com>
2019-11-23 20:08:14 -0300

    financas pe como obj - alguns ajustes - executa apenas despesa - fazer receita

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import StaleElementReferenceException, ElementClickInterceptedException
 from time import sleep
 from datetime import date
 import os
@@ -30,7 +30,6 @@ class FinancasPE:
 
         # creating driver and getting the url
         driver = webdriver.Firefox(fp, options=options)
-        sleep(5)
         return driver
 
     def account_type(self, driver, account):
@@ -39,32 +38,39 @@ class FinancasPE:
             driver.get('http://web.transparencia.pe.gov.br/despesas/despesa-geral/')
         else:
             driver.get('http://web.transparencia.pe.gov.br/receitas/painel-de-receitas/')
+
         sleep(25)
 
         # removing the fixed bar and selecting iframe
         topo = driver.find_element_by_class_name('topo-fixed')
         driver.execute_script("arguments[0].style.position='absolute';", topo)
+
         driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+        print('Url encontrada.')
 
     def select_year(self, driver, year):
-        ops = driver.find_elements_by_xpath("//*[@id='html_anotabelaselector']/select")
-        driver.execute_script("arguments[0].style.display='inline';", ops[0])
+        el_sel = driver.find_element_by_xpath("//*[@id='html_anotabelaselector']/select")
+        driver.execute_script("arguments[0].style.display='inline';", el_sel)
 
-        sel_year_options = driver.find_elements_by_xpath("//*[@id='html_anotabelaselector']/select/option")
-        for option in sel_year_options:
-            try:
-                if option.text == str(year):
-                    option.click()
-                    print('Ano selecionado.')
-                    sleep(10)
-            except StaleElementReferenceException as e:
-                print('resolver ano', e)
+        sleep(20)
+
+        yrs_el = el_sel.find_elements_by_tag_name('option')
+        for yr in yrs_el:
+            if yr.text == str(year):
+                yr.click()
+                break
+        print('ano selecionado')
 
     def enable_options(self, driver):
         # making options visible - by each page refresh the options got hided.
-        ops_xpath = '/html/body/main/section/div/div[5]/div/div/div[2]/div[1]/div[1]/div[4]/div/select'
-        ops = driver.find_elements_by_xpath(ops_xpath)
-        driver.execute_script("arguments[0].style.display='inline';", ops[0])
+        filter_elem = driver.find_element_by_id("filtros_iniciais")
+        driver.execute_script('arguments[0].scrollIntoView(true);', filter_elem)
+
+        sleep(20)
+
+        ug_el = driver.find_elements_by_class_name("chzn-select")
+        driver.execute_script("arguments[0].style.display='inline';", ug_el[3])
+
         options = driver.find_elements_by_xpath("//*[@id='html_selectugtabela']/select/option")
         return options
 
@@ -75,9 +81,9 @@ class FinancasPE:
 
     def export_table(self, driver):
         btn_table = driver.find_element_by_xpath('//*[@id="exportTable"]')
-        driver.execute_script('arguments[0].scrollIntoView(true);', btn_table)
+        # driver.execute_script('arguments[0].scrollIntoView(true);', btn_table)
         driver.execute_script("arguments[0].click();", btn_table)
-        sleep(10)
+        sleep(5)
 
     def get_today(self):
         today = date.today().timetuple()
@@ -95,7 +101,7 @@ class FinancasPE:
         except FileExistsError as e:
             print(e)
         finally:
-            sleep(10)
+            sleep(5)
 
     def verify_files(self):
         files_list = os.listdir(PATH_PAYMENTS)
@@ -108,6 +114,7 @@ class FinancasPE:
         self.account_type(driver, PAYMENTS)
         self.select_year(driver, year)
         uge = self.get_uge_list(driver)
+
         for ug in uge:
             self.verify_files()
             print('Gerando {} de {}.'.format(uge.index(ug), len(uge)))
@@ -115,10 +122,10 @@ class FinancasPE:
             for op in ops:
                 if op.text == ug:
                     op.click()
-                    print('Extraindo planilha {}.'.format(ug))
                     break
-            sleep(10)
             self.export_table(driver)
+            print('Salvando arquivo...')
             self.rename_file(ug, year)
+
         driver.close()
         print('Extração concluída.')
